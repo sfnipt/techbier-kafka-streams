6e1f1a05db5d11398879bd02957836ba46279a9f
silviofankhauser <silvio.fankhauser@ipt.ch>
2022-07-01 14:52:18 +0200

    Aggregate solution

--- src/main/java/ch/ipt/kafka/exercise5/aggregate/KafkaStreamsAggregate.java
+++ src/main/java/ch/ipt/kafka/exercise5/aggregate/KafkaStreamsAggregate.java
@@ -1,4 +1,4 @@
-package ch.ipt.kafka.exercise5.aggregate.solution;
+package ch.ipt.kafka.exercise5.aggregate;
 
 import ch.ipt.kafka.techbier.Payment;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
@@ -6,7 +6,10 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class KafkaStreamsAggregateSolution {
+public class KafkaStreamsAggregate {
 
     @Value("${source-topic-transactions}")
     private String sourceTopic;
@@ -23,7 +26,8 @@ public class KafkaStreamsAggregateSolution {
     @Value("${INITIALS}")
     private String initial;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamsAggregateSolution.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamsAggregate.class);
+
     private static final Serde<String> STRING_SERDE = Serdes.String();
     private static final Serde<Double> DOUBLE_SERDE = Serdes.Double();
     private static final Serde<Payment> PAYMENT_SERDE = new SpecificAvroSerde<>();
@@ -34,21 +38,11 @@ public class KafkaStreamsAggregateSolution {
 
         //compute the total of all transactions per account (e.g. 1: 1632.45, 2: 256.00, ...)
 
-        KStream<String, Double> groupedStream = streamsBuilder.stream(sourceTopic, Consumed.with(STRING_SERDE, PAYMENT_SERDE))
-                .map((key, value) -> new KeyValue<>(
-                        value.getCardType().toString(), value
-                ))
-                .groupByKey()
-                .aggregate(
-                        () -> 0.0,
-                            (key, payment, total) -> total + payment.getAmount(), Materialized.with(Serdes.String(), Serdes.Double())
-                )
-                .toStream()
-                .peek((key, value) -> LOGGER.info("Outgoing record - key " +key +" value " + value));
+        //KStream<String, Double> groupedStream = streamsBuilder.stream(sourceTopic...)
 
-                groupedStream.to(sinkTopic, Produced.with(STRING_SERDE, DOUBLE_SERDE));
+        //TODO...
 
-                LOGGER.info(String.valueOf(streamsBuilder.build().describe()));
+        LOGGER.info(String.valueOf(streamsBuilder.build().describe()));
     }
 
 }
