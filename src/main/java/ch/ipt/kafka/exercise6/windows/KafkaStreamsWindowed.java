f294f8404baf7871e7974e15728b242b0ccd91ba
Christoph Suter <christoph.suter@sanitas.com>
2022-07-01 16:34:07 +0200

    Add Windowed

--- src/main/java/ch/ipt/kafka/exercise6/windows/KafkaStreamsWindowed.java
+++ src/main/java/ch/ipt/kafka/exercise6/windows/KafkaStreamsWindowed.java
@@ -1,22 +1,19 @@
-package ch.ipt.kafka.exercise6.windows.solution;
+package ch.ipt.kafka.exercise6.windows;
 
 import ch.ipt.kafka.techbier.Payment;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.TimeWindows;
-import org.apache.kafka.streams.kstream.WindowedSerdes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
 
 //@Component
-public class KafkaStreamsWindowedSolution {
+public class KafkaStreamsWindowed {
 
     @Value("${source-topic-transactions}")
     private String sourceTopic;
@@ -24,7 +21,7 @@ public class KafkaStreamsWindowedSolution {
     @Value("${INITIALS}")
     private String initial;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamsWindowedSolution.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamsWindowed.class);
 
     @Autowired
     void buildPipeline(StreamsBuilder streamsBuilder) {
@@ -34,13 +31,8 @@ public class KafkaStreamsWindowedSolution {
         KStream<String, Payment> stream = streamsBuilder.stream(sourceTopic);
 
         TimeWindows window = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1));
-        stream
-                .groupBy((k, v) -> v.getCardType().toString())
-                .windowedBy(window)
-                .count()
-                .toStream()
-                .peek((key, value) -> LOGGER.info("Total of transactions in the last minute: key={}, value={}", key, value))
-                .to(sinkTopic, Produced.keySerde(WindowedSerdes.timeWindowedSerdeFrom(String.class, window.sizeMs)));
+
+        //TODO
 
         LOGGER.info(String.valueOf(streamsBuilder.build().describe()));
     }
